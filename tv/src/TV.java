040ac5f5dd001d5df4ecf8c8536fccb1dc56ef26
viniciusfdo <viniciusfo12@gmail.com>
2023-07-26 23:02:08 -0300

    atualizando tv

--- tv/src/TV.java
+++ tv/src/TV.java
@@ -1,5 +1,21 @@
-public class TV {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, TV!");
+public class Tv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+    // estado inicial da tv
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+    
+    public void aumentarVolume(){
+        volume++;
+        
+    }
+    public void diminuirVolume(){
+        volume--;
     }
 }
